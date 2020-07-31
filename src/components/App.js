28a27633a5019283ba5d8bcd15d788d592e134f2
Quintin <quintinrg@protonmail.com>
2020-07-30 22:33:02 -0500

    built basic component outline

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,49 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <Text>Hey what's up</Text>
-      <Text bold>More text please</Text>
-    </div>
+    <main>
+      <Header/>
+      <Search/>
+    </main>
   );
 }
 
-function Text(props) {
-  return (
-    <p style={{ fontWeight: props.bold ? "bold" : "normal" }}>
-      {props.children}
-    </p>
-  );
+function Header(){
+  return(
+    <div>
+    <Brand/>
+    <Nav/>
+    </div>
+  )
+}
+
+function Nav(){
+  return(
+    <nav>
+      <a href="#">Link</a>
+    </nav>
+  )
+}
+function Brand(){
+  return(
+    <h1>Crappy Brand</h1>
+  )
+}
+
+const searchStyles = {
+  fontWeight: 'bold'
+}
+function Search(){
+  return(
+    <ul style={searchStyles}>
+      <Recipe />
+    </ul>
+  )
+}
+function Recipe(){
+  return(
+    <li>"Some Relevant Data"</li>
+  )
 }
 
 export default App;
