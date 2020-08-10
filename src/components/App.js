cdd8850a697d3d887291b6ee7a89235c6eee2289
Quintin <quintinrg@protonmail.com>
2020-08-10 10:45:53 -0500

    grid integration completed. search functionality integrated and then later crippled by search button. Added new recipes for more realistic look.

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,40 @@ import Header from "./Header.js";
 
 let rec_list = [
   { id: 'recipe1', name: 'Hot Dogs', difficulty: 'easy', ingredients: ['meat', 'more meat', 'water'], meals: ['lunch', 'dinner'], image: "/images/hotDog.jpg" }, 
-  { id: 'recipe2', name: 'Roast Duck', difficulty: 'tough', ingredients: ['meat', 'more meat', 'water'], meals: ['lunch', 'dinner'] }, 
-  { id: 'recipe3', name: 'Water', difficulty: 'easy', ingredients: ['water'], meals: ['lunch', 'dinner']}
+  { id: 'recipe2', name: 'Roast Duck', difficulty: 'hard', ingredients: ['duck', 'herbs', 'vegetables'], meals: ['dinner'], image: "/images/roastduck.png" }, 
+  { id: 'recipe3', name: 'Water', difficulty: 'none', ingredients: ['water'], meals: ['always'], image: "/images/water.png"},
+  { id: 'recipe4', name: 'Elixer Of Life', difficulty: 'easy (if still sober)', ingredients: ['chilled glass'], meals: ['any time'], image: "/images/water.png"},
+  { id: 'recipe5', name: 'Cake', difficulty: 'easy', ingredients: ['water', 'flour', 'eggs', 'sugar'], meals: ['lunch', 'dinner'], image: "/images/cake.jpg"},
+  { id: 'recipe6', name: 'Strawberry Dessert', difficulty: 'hard', ingredients: ['strawberries', 'flour', 'eggs', 'sugar', 'cream'], meals: ['dessert'], image: "/images/strawberrydessert.png"},
+  { id: 'recipe7', name: 'Burger', difficulty: 'medium', ingredients: ['meat', 'buns', 'condiments'], meals: ['lunch', 'dinner'], image: "/images/burger.png"}
 ] //Placeholder for actual data
 
 const styles = {
   recipeList: {
     color:"darkgreen",
-    margin:"2em"
+    margin:"1em",
+    padding: 0
   },
   recipe: {
     display: "grid",
-    // gridColumnStart: 2,
-    margin:"1em",
+    gridTemplateAreas: `
+      "icon header      difficulty" 
+      "icon ingredients meals" 
+      "icon ingredients select`,
+    gridTemplateColumns: "15vw 1fr 15vw",
+    border: "1px solid #eee",
+    borderRadius: ".5rem",
+    marginBottom: ".5rem",
+    padding: ".5rem 0",
+    alignItems: "center"
   },
   searchBar: {
     backgroundColor: 'lightgrey',
     margin: '1em'
   },
-  recipeGrid: {
-    
-
+  image: {
+    maxWidth: "250px"
   }
-
 }
 
 
@@ -38,28 +49,44 @@ function App() {
   );
 }
 
+const cleanTerm = term => term.toLowerCase().trim()
 
 function Search(){
+  const [terms, setTerms] = React.useState("")
+  const [recipes, setRecipes] = React.useState(rec_list)
+
+  console.log("refreshed")
+
+  const filterList = () => {
+    const filtered = rec_list.filter(recipe => cleanTerm(recipe.name).startsWith(cleanTerm(terms)) ) 
+    setRecipes(filtered)
+  }
+
   return(
     <div>
-        <input style={styles.searchBar} placeholder="  Search"/>
-        <button><p style={{ transform: "rotate(45deg)", margin: 0}}>O--</p></button>
-      <ul>
-        <RecipeList />
-      </ul>
+        <input style={styles.searchBar} placeholder="  Search" value={terms}  onChange={(e) => setTerms(e.target.value)}/>
+        <button onClick={filterList}><p style={{ transform: "rotate(45deg)", margin: 0}}>O--</p></button>
+        <RecipeList recipes={recipes} />
     </div>
   )
 }
-function RecipeList(){
+
+function RecipeList(props){
   return(
     <ul style={styles.recipeList}>
-      {rec_list.map(recipe => (<Recipe key={recipe.id} recipe={recipe}/>))}
+      {props.recipes.map(recipe => (<Recipe key={recipe.id} recipe={recipe}/>))}
     </ul>
   )
 }
 
 function Recipe (props){  
-return (<li style={styles.recipe}><header style={styles.recipeGrid}>{props.recipe.name}</header><img src={props.recipe.image} style={styles.recipeGrid}/><p style={styles.recipeGrid}>Ingredients: {props.recipe.ingredients.join(', ')}</p><p style={styles.recipeGrid}>difficulty: {props.recipe.difficulty}</p></li>)
+return (<li style={styles.recipe}>
+  <header style={{ gridArea: 'header'}}>{props.recipe.name}</header>
+  <img src={props.recipe.image} style={{ gridArea: 'icon' }} style={styles.image}/>
+  <p style={{ gridArea: 'ingredients'}}>Ingredients: {props.recipe.ingredients.join(', ')}</p>
+  <p style={{gridArea: 'difficulty'}}>difficulty: {props.recipe.difficulty}</p>
+  <p style={{gridArea: 'meals'}}>meals: {props.recipe.meals.join(', ')}</p>
+  </li>)
 }
 
 export default App;
