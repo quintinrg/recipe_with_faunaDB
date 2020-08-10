61e6a120ceb0215c9cb7eb385098b2aa540bab27
Quintin <quintinrg@protonmail.com>
2020-08-09 21:17:40 -0500

    added local image and attempting grid integration

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header.js";
 
 let rec_list = [
-  { id: 'recipe1', name: 'Hot Dogs', difficulty: 'easy', ingredients: ['meat', 'more meat', 'water'], meals: ['lunch', 'dinner'], image: "link_to_image" }, 
+  { id: 'recipe1', name: 'Hot Dogs', difficulty: 'easy', ingredients: ['meat', 'more meat', 'water'], meals: ['lunch', 'dinner'], image: "/images/hotDog.jpg" }, 
   { id: 'recipe2', name: 'Roast Duck', difficulty: 'tough', ingredients: ['meat', 'more meat', 'water'], meals: ['lunch', 'dinner'] }, 
   { id: 'recipe3', name: 'Water', difficulty: 'easy', ingredients: ['water'], meals: ['lunch', 'dinner']}
 ] //Placeholder for actual data
@@ -13,12 +13,19 @@ const styles = {
     margin:"2em"
   },
   recipe: {
-    margin:"1em"
+    display: "grid",
+    // gridColumnStart: 2,
+    margin:"1em",
   },
   searchBar: {
     backgroundColor: 'lightgrey',
     margin: '1em'
+  },
+  recipeGrid: {
+    
+
   }
+
 }
 
 
@@ -52,7 +59,7 @@ function RecipeList(){
 }
 
 function Recipe (props){  
-return (<li style={styles.recipe}><header>{props.recipe.name}</header><img src={props.recipe.image}/><p>Ingredients: {props.recipe.ingredients.join(', ')}</p><p>difficulty: {props.recipe.difficulty}</p></li>)
+return (<li style={styles.recipe}><header style={styles.recipeGrid}>{props.recipe.name}</header><img src={props.recipe.image} style={styles.recipeGrid}/><p style={styles.recipeGrid}>Ingredients: {props.recipe.ingredients.join(', ')}</p><p style={styles.recipeGrid}>difficulty: {props.recipe.difficulty}</p></li>)
 }
 
 export default App;
